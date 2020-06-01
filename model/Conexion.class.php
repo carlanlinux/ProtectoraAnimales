5726b78a0f3cb4ead23a18ab1496243f2c18be8d
Carlos M. de Lucas <carlanlinux@gmail.com>
2020-06-01 20:42:04 +0200

    fix: Add options in PDO connection

--- model/Conexion.class.php
+++ model/Conexion.class.php
@@ -7,13 +7,14 @@ class Conexion
     const DB_USER = "root";
     const DB_PASS = "root";
     const DB_NAME = "protectora_animales";
+    const OPC = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
     //$dsn = 'mysql:host=localhost;dbname=protectora_animales;port=8889';
     const DSN = "mysql:host=" . self::DB_SERVER . ";dbname=" . self::DB_NAME . ";";
 
 
     protected function setConnection() {
         try {
-           $pdo = new PDO(self::DSN, self::DB_USER, self::DB_PASS);
+           $pdo = new PDO(self::DSN, self::DB_USER, self::DB_PASS, self::OPC);
             return $pdo;
         }
         catch (Exception $e) {
